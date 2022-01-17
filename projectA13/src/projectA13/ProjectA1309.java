6db6b0848579032b44e3c267744ffd81fd3a8acb
Taeyoung92 <marine4391@naver.com>
2022-01-17 21:45:49 +0900

    Day_13

---
+++ projectA13/src/projectA13/ProjectA1309.java
@@ -0,0 +1,26 @@
+package projectA13;
+
+import java.io.File;
+
+public class ProjectA1309 {
+	public static void listDirectory(File dir) {
+		System.out.println("-----" + dir.getPath() + "의 서브 리스트입니다.");
+		File[] subFiles = dir.listFiles();
+		for(int i = 0; i<subFiles.length; i++) {
+			File f = subFiles[i];
+			long t = f.lastModified();
+			System.out.print(f.getName());
+			System.out.print(" 파일 크기 : " + f.length() + " ");
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		File f1 = new File("c:\\windows\\system.ini");
+		System.out.println(f1.getPath() + ", " + f1.getParent() + ", " + f1.getName());
+		
+		listDirectory(new File("c:\\temp2"));
+
+	}
+
+}
